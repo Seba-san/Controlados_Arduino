0a221c4a33950ecde8a77c34b937befb4ebc555e
Seba <sansoni.sebas@gmail.com>
2018-04-12 17:56:42 -0300

    No hace el calculo de tiempos correctamente, por esto guardo este punto y abro una rama, asi limpio el programa

--- Trabajo_Final_Controlados/Trabajo_Final_Controlados.ino
+++ Trabajo_Final_Controlados/Trabajo_Final_Controlados.ino
@@ -25,13 +25,15 @@ Controlados controlados1;
 #define interruptOFF bitClear(SREG,7)//desactiva las interrupciones globales
 #define cantMarcasEncoder 8 //Es la cantidad de huecos que tiene el encoder de
                             //cada motor.
+#define FsEncoders 2000
 #define preescaler 32
 #define cota 382//cota=32 hace que de 0 a aprox 100rpm asuma que la velocidad es cero.
 #define fclkio 16300000//DEFINIR UNIDAD //Frecuencia del nano
+#define fclkio2 1000000000/16300000
 // ############# Probandos cosas locas
 #define NOP __asm__ __volatile__ ("nop\n\t")
 #define SalidaTest 3
-bool estado=1;
+bool estado=0,estado2=0;
 //################
 
 
@@ -51,16 +53,24 @@ unsigned char preescalerPorSoft=0;//Esta variable me permite aumentar por soft e
 int TCNT2anterior=0;//Valor anterior del contador (para corregir la medición)
 int TCNT2actual=0;//Almaceno el valor del timer para que no me jodan posibles
                   //actualizaciones.
-float bufferVel[2*cantMarcasEncoder];//buffer donde almaceno las últimas
-                                     //velocidades calculadas.
+
+int    cantOVerflow_actual=0;     //Valor anterior del contador (para corregir la medición), correspondiente al TCNT2anterior.          
+unsigned long  bufferVel[2*cantMarcasEncoder];//buffer donde almaceno las últimas velocidades calculadas.
+unsigned long cuco=0;
 float velAngular=0;//última velocidad angular calculada. Lo separo del vector
                    //bufferVel para que no haya problemas por interrumpir en
                    //medio de una actualización de éste.
 float velDeseada=0;//Empieza detenido el motor.
 float error[2]={0,0},u[2]={0,0};
 
+
+// Variables basura
+
+int Bandera=0; // bandera para administrar procesos fuera de interrupciones  
+
 // # Declarando Funciones
-void configTimer2Contador(void);
+//void configTimer2Contador(void);
+void medirVelocidad(unsigned char);
 
 void setup() {
   interruptOFF; // se desactivan las interrupciones para configurar.
@@ -69,27 +79,58 @@ void setup() {
   controlados1.configPinesMotores();
   controlados1.modoStop();
   controlados1.configTimerMotores();
- 
-  //controlados1.configTimer2Contador();//Configuro el timer2 como contador con interrupción $VER: No estan en las librerias
+  controlados1.configTimer2Contador((long) FsEncoders);//Configuro el timer2 como contador con interrupción. La frecuencia va desde 500000 hasta 1997.   
   controlados1.actualizarDutyCycleMotores(70,30);
   controlados1.modoAdelante();
 
   //Interrupciones por estado en pin para lectura de los encoders:
+ 
   bitWrite(PCICR,PCIE1,1); // Pin Change Interrupt Control Register ; Bit 1 – PCIE1: Pin Change Interrupt Enable 1; PCINT[14:8]
+  bitWrite(PCMSK1,PCINT8,1); // PCINT8 correspondo a A0.
+  bitWrite(PCIFR,PCIF1,1);// Limpio la bandera
+  
   //Ver si esto lo ponemos en la librería$
   interruptON;//Activo las interrupciones
   pinMode(A0, INPUT);
   // $Prueba
   pinMode(SalidaTest, OUTPUT);
-   configTimer2Contador(); // la agrego a mano.
+   //configTimer2Contador(); // la agrego a mano.
 }
 
 void loop() {
+  //OCR2A*cantOVerflow+TCNT2actual-TCNT2anterior)*preescaler
   NOP;
+if (bitRead(Bandera,0)){
+  //Serial.println  (Bandera,BIN);
+  bitWrite(Bandera,0,0);
+ // Serial.println(bufferVel[2*cantMarcasEncoder-1],DEC); 
+  Serial.print(cantOVerflow,DEC);
+  Serial.print(" ");
+  Serial.print(TCNT2actual,DEC);
+  Serial.print(" ");
+  Serial.print(TCNT2anterior,DEC); 
+  Serial.print(" ");
+  Serial.print(cantOVerflow_actual,DEC); 
+  Serial.print(" ");
+  Serial.println(cuco,DEC);
+  } 
+  if (bitRead(Bandera,1)){
+  // Serial.println  (Bandera,BIN);
+  bitWrite(Bandera,1,0);
+  medirVelocidad(0);   
+
+  } 
+  if (bitRead(Bandera,2)){
+  //  Serial.println  (Bandera,BIN);
+  bitWrite(Bandera,2,0);
+  medirVelocidad(1);      
+  } 
+   
+  /*
   Serial.print(TCNT2,DEC);
     Serial.print(" ");
     Serial.println(" cora ");
-
+*/
 }
 /*
 void serialEvent() {
@@ -121,16 +162,23 @@ void serialEvent() {
 */
 ISR (TIMER2_COMPA_vect){//Interrupción por Timer2 para definir frec de muestreo del sist cte; Resetea con el valor de comparacion del A
   //COMPLETAR$
-  
-  /*
-   * cantOVerflow++;
+  bitWrite(Bandera,0,1);
+  estado=!estado;
+  if (estado){
+    estado2=!estado2;
+  digitalWrite(SalidaTest,estado2);
+  }
+   cantOVerflow++;
   if(cantOVerflow>cota){
-    medirVelocidad(0);//Llamo a la rutina de medición de vel indicándole
+   bitWrite(Bandera,1,1);
+   TCNT2actual=TCNT2;
+    //medirVelocidad(0);//Llamo a la rutina de medición de vel indicándole
                       //que pasó demasiado tiempo y que tiene que asumir
                       //que la velocidad es 0.
   }
   preescalerPorSoft++;//Esta variable me permite aumentar por soft el preescalador del timer 
-  if(preescalerPorSoft>=1){
+ /*
+  if(preescalerPorSoft>=10){
     preescalerPorSoft=0;
     error[1]=velDeseada-velAngular;//ek=wdeseado-wmedido
     u[1]=ControladorMotor(error[0],error[1],u[0]);
@@ -139,17 +187,19 @@ ISR (TIMER2_COMPA_vect){//Interrupción por Timer2 para definir frec de muestreo
     if(velDeseada==0){u[1]=0;}//Esta parte permitiría apagar el motor cuando funciona el control
     //Poner de nuevo cuando terminen las pruebas$$
     //Serial.println(velAngular);//Le envío a la compu el valor de velocidad actual
-
+/*
     //PARTE DE PRUEBA, BORRAR!!!!$$
     //Hay que fijar la cant de cifras!!!!!$$
-//    Serial.print(cantOVerflow);
-//    Serial.print(' ');//Caracter para separar datos (space=SP=dec32=0x20)
-//    Serial.println(0);//Simulo que envío la señal de control (siempre 0)
+    Serial.print(cantOVerflow);
+    Serial.print(' ');//Caracter para separar datos (space=SP=dec32=0x20)
+    Serial.println(0);//Simulo que envío la señal de control (siempre 0)
+    
   }
   */
-  estado=!estado;
-  digitalWrite(SalidaTest,estado);
-  Serial.println(TCNT2,DEC);
+  
+ // Serial.println(TCNT2,DEC);
+ //Serial.println(velAngular,DEC);     
+      
 }
 /*
 float ControladorMotor(float ek_1,float ek, float uk_1)
@@ -165,28 +215,46 @@ float ControladorMotor(float ek_1,float ek, float uk_1)
     //FALTA LA MODIIFCACION EFECTIVA DE LA VELOCIDAD DEL MOTOR.
     return uk;
 }
-
+*/
 ISR(PCINT1_vect){
-  medirVelocidad(1);
+ /*
+  * Hay que verificar donde fue el cambio de estado, porque al tener 2 ruedas, no se sabe de donde provino (habria que hacer una comparacion manual)
+  */
+  TCNT2actual=TCNT2;
+  cantOVerflow_actual=cantOVerflow;
+    //medirVelocidad(1);
+    bitWrite(Bandera,2,1);
+  
 }
+
 void medirVelocidad(unsigned char interrupcion)
 {
   //Arranco determinando el conteo real de ciclos del timer2 tranascurridos desde la última
   //interrupción:
-  TCNT2actual=TCNT2;//Almaceno enseguida el valor del timer para que no cambie mientras hago
+  //TCNT2actual=TCNT2;//Almaceno enseguida el valor del timer para que no cambie mientras hago
                     //las cuentas.
+  cantOVerflow=0;//Reseteo el valor de cantidad de interrupciones ocurridas por timer 2
+  
   int suma=0;
   //Corro los valores de w en el buffer un lugar y voy sumando los valores para después
   //calcular el promedio de velocidades:
-  for(int k=0;k++;k<2*cantMarcasEncoder-1)
+  /*
+  for(int k=0;k++;k<(2*cantMarcasEncoder-1))
   {
     bufferVel[k]=bufferVel[k+1];//Desplazamiento a la derecha de los datos del buffer
     suma=suma+bufferVel[k];
   }
+  */
   //Al terminar el bucle bufferVel tiene los últimos dos valores iguales (los dos de más a
   //la izquierda). Esto cambia a continuación con la actualización del valor más a la derecha:
   if(interrupcion){
-    bufferVel[2*cantMarcasEncoder-1]=60*fclkio/(2*cantMarcasEncoder*preescaler*(TCNT2actual+cantOVerflow*OCR2A-TCNT2anterior));
+    interruptOFF;
+    cuco=(OCR2A*cantOVerflow_actual*32);// Es para contar ciclos
+    interruptON;
+    //bufferVel[2*cantMarcasEncoder-1]=preescaler*(TCNT2actual+cantOVerflow*OCR2A-TCNT2anterior)*fclkio2;// Es para contar ciclos
+    //bufferVel[2*cantMarcasEncoder-1]=fclkio/(2*cantMarcasEncoder*preescaler*(TCNT2actual+cantOVerflow*OCR2A-TCNT2anterior));
+                              //Unidad de medición: ciclos/seg.
+    //bufferVel[2*cantMarcasEncoder-1]=60*fclkio/(2*cantMarcasEncoder*preescaler*(TCNT2actual+cantOVerflow*OCR2A-TCNT2anterior));
                               //Unidad de medición: rpm.
   }
   else{
@@ -195,17 +263,21 @@ void medirVelocidad(unsigned char interrupcion)
     //(cuanto cantOverflow supera la cota permitida).
     bufferVel[2*cantMarcasEncoder-1]=0;
   }
-  cantOVerflow=0;//Reseteo el valor de cantidad de interrupciones ocurridas por timer 2
+
+
   TCNT2anterior=TCNT2actual;//Ahora el valor actual pasa a ser el anterior de la próxima
                             //interrupción.
+                            /*
   suma=suma+bufferVel[2*cantMarcasEncoder-1];
   velAngular=suma/(2*cantMarcasEncoder);//Actualizo el valor de velocidad medida como el
                                         //promedio de las últimas mediciones (todas las del
                                         //buffer).
+                             
   //Obs: para el correcto funcionamiento de la rutina se requiere que no haya interrupción
   //por overflow en el timer 2 durante la ejecución de estas instrucciones
+  */
 }
-
+/*
 void tic(){
   ticc=micros();
 }
@@ -214,17 +286,4 @@ void toc(){
   tocc=micros()-ticc;
 }
 */
-void configTimer2Contador(void){
-  //Clear Timer on Compare Match (CTC) Mode  (WGM2[2:0] = 2) (pag 195)
-  //bitWrite(TCCR2A,1,0); // Modo CTC
-  
-  TCCR2A=0b00000010;
- // ## prescaler en 1:32
-  //bitWrite(TCCR2B,0,1); // Prescaler 32
-  //bitWrite(TCCR2B,1,1); // Prescaler 32
-   TCCR2B|=0b00000011;
-  OCR2A=100;//51 // De esta forma genera un overflow a 10Kz, la ecuacion es: Freq=16.3 10^6 / (32 * OCR2A), osea que: OCR2A=16.3 10^6 / (32 * Freq)
-  OCR2B=0; // No afecta, por el modo de configuracion CTC.
-  bitWrite(TIMSK2,1,1); // TBit 1 – OCIEA: Timer/Counter2, Output Compare A Match Interrupt Enable
 
-  }
