1c5a3f70ecdcd2db50a1c2ba5f3f7e6ac2803b01
Seba <sansoni.sebas@gmail.com>
2018-04-19 12:54:48 -0300

    se sacaron comentarios de funciones, y variables que no se usaban, esta rama sirve para ir sacando cosas que no se usan.

--- Trabajo_Final_Controlados/Trabajo_Final_Controlados.ino
+++ Trabajo_Final_Controlados/Trabajo_Final_Controlados.ino
@@ -23,24 +23,17 @@ const unsigned long _OCR2A = 250;
 #define SalidaTest 3
 #define SalidaTest2 4
 #define SalidaTest3 5
-bool estado=0,estado2=0,estado3=0, estado4=0;
- //unsigned long suma=0;//float suma=0;
+
+
  
 // #   #   #   # Variable Basura
-int Bandera=0; // bandera para administrar procesos fuera de interrupciones 
-float b, freq; 
-unsigned long cuenta;    
+int Bandera=0; // bandera para administrar procesos fuera de interrupciones    
 //################
 
 // #   #   #   # Variables
 unsigned char trama_activa=0;//Lo pongo en unsigned char para que ocupe 1 byte (int ocupa 2)                           
 int cantOVerflow=0;//Variable que almacena la cantidad de veces que se desbordó el timer2 hasta que vuelvo a tener interrupción por pin de entrada. Esto permite realizar la medición de 
                   //tiempos entre aujeros del encoder.                                                                              
-/*
-int TCNT2anterior=0;//Valor anterior del contador (para corregir la medición)
-int TCNT2actual=0;//Almaceno el valor del timer para que no me jodan posibles actualizaciones.                
-int cantOVerflow_actual=0;     //Valor anterior del contador (para corregir la medición), correspondiente al TCNT2anterior.          
-*/
 unsigned long TCNT2anterior=0;//Valor anterior del contador (para corregir la medición)
 unsigned long TCNT2actual=0;//Almaceno el valor del timer para que no me jodan posibles actualizaciones.                
 unsigned long cantOVerflow_actual=0;     //Valor anterior del contador (para corregir la medición), correspondiente al TCNT2anterior. 
@@ -71,11 +64,7 @@ Serial.begin(115200); // Si se comunica a mucha velocidad se producen errores (q
 
   interruptON;//Activo las interrupciones
   pinMode(A0, INPUT);
-  // $Prueba
-  pinMode(SalidaTest, OUTPUT);
-  pinMode(SalidaTest2, OUTPUT);
-  pinMode(SalidaTest3, OUTPUT);
-
+ 
 }
 
 void loop() {
@@ -84,7 +73,6 @@ void loop() {
 if (bitRead(Bandera,0)){ // timer 2 overflow
   } 
   if (bitRead(Bandera,1)){ // Entra cuando no registra cambio en la entrada
-  // Serial.println  (Bandera,BIN);
   bitWrite(Bandera,1,0);
   medirVelocidad(0);   
 
@@ -127,27 +115,19 @@ timer_interrupt();
 
 }
 void timer_interrupt(){ // se utiliza esta funcion ya que permite llamarla desde otras funciones. Si lo haces desde la interrupcion, no podes.
-  //estado2=!estado2;
-  //digitalWrite(SalidaTest,estado2);
+  
   cantOVerflow++;
   if(cantOVerflow>cota){
    bitWrite(Bandera,1,1); //medirVelocidad(0);//Llamo a la rutina de medición de vel indicándole que pasó demasiado tiempo y que tiene que asumir que la velocidad es 0.                                        
   }
   bitWrite(Bandera,0,1);
-  //estado=!estado;
-  //if (estado){
-   // estado2=!estado2;
-  //digitalWrite(SalidaTest,estado2);
-  //}
-  //digitalWrite(SalidaTest,0);
+  
 }
 ISR(PCINT1_vect){
  /*
   * Hay que verificar donde fue el cambio de estado, porque al tener 2 ruedas, no se sabe de donde provino (habria que hacer una comparacion manual)
   */
- // estado3=!estado3;
- // if (estado3){
- //digitalWrite(SalidaTest2,estado3);
+ 
   TCNT2anterior=TCNT2actual;//Ahora el valor actual pasa a ser el anterior de la próxima interrupción.   
   if (bitRead(TIFR2,1)){ // me fijo si hay overflow
   timer_interrupt();
@@ -157,10 +137,6 @@ ISR(PCINT1_vect){
   cantOVerflow_actual=cantOVerflow; 
   cantOVerflow=0;
   bitWrite(Bandera,2,1);   //medirVelocidad(1); 
-  //estado=!estado;
-   //digitalWrite(SalidaTest2,0);
-  //}
-  
 }
 
 void medirVelocidad(unsigned char interrupcion)
@@ -183,37 +159,12 @@ unsigned long suma=0;
      t=cantOVerflow_actual*OCR2A;
      tmh=TCNT2actual-TCNT2anterior+t; // Ver problemas de variables
     bufferVel[2*cantMarcasEncoder-1]=long(preescaler)*(tmh);
-    
-    //bufferVel[2*cantMarcasEncoder-1]=(long(preescaler)*(TCNT2actual+cantOVerflow_actual*long(OCR2A)-TCNT2anterior));
-     //bufferVel[2*cantMarcasEncoder-1]=float(fclkio)/(float(preescaler)*(TCNT2actual+cantOVerflow_actual*float(OCR2A)-TCNT2anterior));
-   // bufferVel[2*cantMarcasEncoder-1]=preescaler*(TCNT2actual+cantOVerflow*OCR2A-TCNT2anterior)*fclkio2;// Es para contar ciclos
-    //bufferVel[2*cantMarcasEncoder-1]=fclkio/(2*cantMarcasEncoder*preescaler*(TCNT2actual+cantOVerflow*OCR2A-TCNT2anterior));
-                              //Unidad de medición: ciclos/seg.
-   // bufferVel[2*cantMarcasEncoder-1]=60.0*float(fclkio)/(float(2*cantMarcasEncoder*preescaler)*(float(cantOVerflow_actual*OCR2A)-TCNT2anterior+TCNT2actual));
-                              //Unidad de medición: rpm.
   }
   else{
      bufferVel[2*cantMarcasEncoder-1]=0;
   }   
 
-// ################## Parte del codigo funcional con la version: commit b162d5d7e01e02d2daadd90df3ba0800cd0464f5
-  freq=float(F_CPU)/(bufferVel[15]+bufferVel[14]);
-  b=100-freq;
-  if (abs(b)>1){
-    //estado4=!estado4;
-     // digitalWrite(SalidaTest3,estado4);
-  aux[0]=TCNT2anterior;
-  aux[1]=TCNT2actual;
-  aux[2]=cantOVerflow_actual;   
-  aux[0]= bufferVel[15]+bufferVel[14]; 
-    aux[1]= cuenta; 
-    cuenta=0;                 
-EnviarTX(2,'a',aux);
-  }
-  else{cuenta++;}
-EnviarTX_online( bufferVel[2*cantMarcasEncoder-1]);
-
-// ################## Parte del codigo funcional con la version: commit b162d5d7e01e02d2daadd90df3ba0800cd0464f5
+suma=suma+bufferVel[2*cantMarcasEncoder-1];
 }
 
 // Ver como mejorar sustancialmente esta funcion. Esta media fea.
